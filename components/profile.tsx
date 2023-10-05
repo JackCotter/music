81c2901c7093277822aeb1bbff285bebb1c691ad
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-10-04 23:06:46 -0700

    added recording

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@mui/material";
+import { Button, Slider, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
+import { start } from "repl";
 import * as Tone from "tone";
 
 const fakeQueryData = [
@@ -8,22 +9,22 @@ const fakeQueryData = [
 ];
 
 const Profile = () => {
+  Tone.Transport.debug = true;
   const [players, setPlayers] = useState<Tone.Players | null>(null);
-  // const [transport, setTransport] = useState<Tone.Transport | null>(null);
+  const [recorder, setRecorder] = useState<MediaRecorder | null>(null);
+
   useEffect(() => {
     let playerDict: { [key: string]: string } = {};
     fakeQueryData.forEach((url, index) => {
       playerDict[index.toString()] = url;
     });
-    const player: Tone.Players = new Tone.Players(playerDict, () =>
-      setPlayers(player)
+    const players: Tone.Players = new Tone.Players(playerDict, () =>
+      setPlayers(players)
     ).toDestination();
-    // play as soon as the buffer is loaded
-    // player.autostart = false;
     return () => {
-      if (player) {
+      if (players) {
         console.log("disposed");
-        // player.dispose(); // Clean up the player
+        players.dispose(); // Clean up the player
       }
     };
   }, []);
@@ -33,27 +34,88 @@ const Profile = () => {
       for (let i = 0; i < fakeQueryData.length; i++) {
         players.player(i.toString()).start(i.toString());
       }
+      if (players.has("Recording")) {
+        console.log("recording");
+        players.player("Recording").start();
+      }
     }
   };
 
   const stopAudio = () => {
     if (players && players.loaded) {
       players.stopAll();
-      // players.sto?p("+1");
     } else {
       console.log("not loaded");
     }
   };
 
+  const startRecording = () => {
+    Tone.start();
+    let recorder: MediaRecorder;
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      recorder = new MediaRecorder(stream);
+      setRecorder(recorder);
+      recorder.start();
+      startAudio();
+    });
+  };
+
+  const stopRecording = () => {
+    if (recorder) {
+      recorder.stop();
+      recorder.ondataavailable = (e) => {
+        players?.add("Recording", URL.createObjectURL(e.data), () =>
+          console.log(players?.player("Recording").get())
+        );
+      };
+      console.log(players?.player("0").get());
+      stopAudio();
+    }
+  };
+
   return (
-    <>
-      <Button variant="contained" onClick={() => startAudio()}>
-        Start Audio
-      </Button>
-      <Button variant="contained" onClick={() => stopAudio()}>
-        Stop Audio
-      </Button>
-    </>
+    <Stack direction="column" spacing={2}>
+      <Stack direction="row" spacing={2}>
+        <Button variant="contained" onClick={() => startAudio()}>
+          Start Audio
+        </Button>
+        <Button variant="contained" onClick={() => stopAudio()}>
+          Stop Audio
+        </Button>
+        <Button variant="contained" onClick={() => startRecording()}>
+          Start Recording
+        </Button>
+        <Button variant="contained" onClick={() => stopRecording()}>
+          Stop Recording
+        </Button>
+      </Stack>
+      {/* <Stack direction="column" spacing={2}>
+        {fakeQueryData.map((url, index) => {
+          return (
+            <Stack direction="column" spacing={2}>
+              <Slider
+                key={index}
+                value={
+                  startTime && players
+                    ? (players.player(index.toString()).buffer. -
+                        startTime) /
+                      players.player(index.toString()).buffer.duration
+                    : 0
+                }
+              />
+              <div>
+                {startTime && players
+                  ? players.player("1").immediate() - startTime
+                  : 0}
+              </div>
+              <div>
+                {players ? players.player(index.toString()).buffer.duration : 0}
+              </div>
+            </Stack>
+          );
+        })}
+      </Stack> */}
+    </Stack>
   );
 };
 
