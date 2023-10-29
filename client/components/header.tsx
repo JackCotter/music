c08d37edbacde6716f0fbc6247d3b3f26b581120
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-10-28 23:20:51 -0700

    added iconbutton linking to project creation

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import styles from "@/styles/components/header.module.scss";
 import LoginModal from "./modals/loginModal";
 import { useAuthContext } from "@/contexts/authContext";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import Link from "next/link";
 
 const Header: React.FC = () => {
   const [isLoginModalOpen, setIsLoginModalOpen] =
@@ -18,6 +20,11 @@ const Header: React.FC = () => {
           <Typography variant="h6" component="div" className={styles.title}>
             Track Track er
           </Typography>
+          <Link href="/project-create">
+            <IconButton color="secondary" className={styles.newProjectButton}>
+              <AddCircleOutlineIcon className={styles.newProjectIcon} />
+            </IconButton>
+          </Link>
           {!authContext.isAuthenticated && (
             <Button
               color="secondary"
---
+++ client/pages/project-create.tsx
@@ -0,0 +1,7 @@
+import { Stack } from "@mui/material";
+
+const ProjectCreate = () => {
+  return <Stack direction="column">hello</Stack>;
+};
+
+export default ProjectCreate;
