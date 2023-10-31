8f010fae3ab6cadbd11245402810433bfe37f8d3
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-10-31 12:53:28 -0700

    fixed theming issues by turning on dark mode. smh

--- client/styles/theme.ts
+++ client/styles/theme.ts
@@ -3,6 +3,7 @@ import themeColors from "../styles/pages/app.module.scss";
 
 export const darkThemeOptions: ThemeOptions = {
   palette: {
+    mode: "dark",
     primary: {
       main: themeColors.primary,
     },
@@ -16,68 +17,5 @@ export const darkThemeOptions: ThemeOptions = {
       primary: themeColors.lightText,
       secondary: themeColors.lightText,
     },
-  },
-  components: {
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          "& .MuiFormLabel-root": {
-            color: themeColors.lightText,
-          },
-          "& .MuiOutlinedInput-notchedOutline": {
-            borderColor: themeColors.lightText,
-          },
-          "& .MuiInputBase-input": {
-            color: themeColors.lightText,
-          },
-        },
-      },
-    },
-    MuiSelect: {
-      styleOverrides: {
-        root: {
-          ".MuiOutlinedInput-notchedOutline": {
-            borderColor: "rgba(228, 219, 233, 0.25)",
-          },
-          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-            borderColor: "rgba(228, 219, 233, 0.25)",
-          },
-          "&:hover .MuiOutlinedInput-notchedOutline": {
-            borderColor: "rgba(228, 219, 233, 0.25)",
-          },
-          "&:before .MuiOutlinedInput-notchedOutline": {
-            borderColor: "rgba(228, 219, 233, 0.25)",
-          },
-          ".MuiSvgIcon-root ": {
-            fill: "white !important",
-          },
-          },
-        },
-      },
-    MuiDivider: {
-      styleOverrides: {
-        root: {
-          backgroundColor: themeColors.lightText,
-        },
-      },
-    },
-    //       "& .MuiInputBase-input": {
-    //         color: themeColors.lightText,
-    //       },
-    //       "& .MuiSelect-icon": {
-    //         color: themeColors.lightText,
-    //       },
-    //       "& .MuiInputLabel-root": {
-    //         color: themeColors.lightText,
-    //       },
-    //       "& .MuiOutlinedInput-notchedOutline": {
-    //         color: themeColors.lightText,
-    //       },
-    //       "& .MuiListItem-root": {
-    //         color: themeColors.lightText,
-    //       },
-    //     },
-    //   },
-    // },
   },
 };
