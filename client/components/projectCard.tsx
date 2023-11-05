301b9e0bb1b4f2bfd7c53dfc7ce2bd29c6c4f0e9
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-11-05 11:17:49 -0800

    Added strict icons, tooltips and hiding if no inst

--- client/components/projectCard.tsx
+++ client/components/projectCard.tsx
@@ -4,6 +4,7 @@ import {
   Chip,
   IconButton,
   Stack,
+  Tooltip,
   Typography,
 } from "@mui/material";
 import styles from "@/styles/components/projectCard.module.scss";
@@ -14,6 +15,8 @@ import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import StopIcon from "@mui/icons-material/Stop";
 import Link from "next/link";
 import { maxNCharacters } from "@/utils/stringUtils";
+import LockIcon from "@mui/icons-material/Lock";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
 
 interface ProjectCardProps {
   project: Project;
@@ -42,6 +45,8 @@ const ProjectCard = ({
 
   useEffect(() => {
     if (project.projectid === undefined) return;
+    console.log(project);
+    console.log(project.lookingfor?.length);
     populatePlayers(project.projectid, setTrackList, setPlayers);
   }, [project.projectid]);
 
@@ -58,36 +63,58 @@ const ProjectCard = ({
                 {project.projectname}
               </Typography>
             </Link>
-            <Typography className={styles.username} variant="body2">
-              {project.username}
-            </Typography>
-            <div>
-              <IconButton
-                color="secondary"
-                onClick={() =>
-                  isAudioPlaying
-                    ? stopAudio(players, setIsAudioPlaying)
-                    : startAudio(players, trackList, setIsAudioPlaying)
+            <Stack
+              className={styles.usernameAndPlayButton}
+              direction="row"
+              spacing={1}
+            >
+              <Typography className={styles.username} variant="body2">
+                {project.username}
+              </Typography>
+              <div>
+                <IconButton
+                  color="secondary"
+                  onClick={() =>
+                    isAudioPlaying
+                      ? stopAudio(players, setIsAudioPlaying)
+                      : startAudio(players, trackList, setIsAudioPlaying)
+                  }
+                >
+                  {isAudioPlaying ? <StopIcon /> : <PlayArrowIcon />}
+                </IconButton>
+              </div>
+            </Stack>
+          </Stack>
+          {project.lookingfor && project.lookingfor.length && (
+            <Stack className={styles.instrumentRow} direction="row" spacing={1}>
+              <div className={styles.instruments}>
+                <Stack direction="row" spacing={1}>
+                  {project.lookingfor?.map((instrument) => (
+                    <Chip
+                      key={instrument}
+                      color={
+                        highlightedInstruments.includes(instrument)
+                          ? "primary"
+                          : "default"
+                      }
+                      onClick={() => onInstrumentClick(instrument)}
+                      label={instrument}
+                    />
+                  ))}
+                </Stack>
+              </div>
+              <Tooltip
+                placement="top"
+                title={
+                  project.lookingforstrict
+                    ? "The project creator has enabled strict mode. Only the selected instruments can be contributed."
+                    : "The project creator has not enabled strict mode. Any instrument can be contributed."
                 }
               >
-                {isAudioPlaying ? <StopIcon /> : <PlayArrowIcon />}
-              </IconButton>
-            </div>
-          </Stack>
-          <Stack className={styles.instrumentRow} direction="row" spacing={1}>
-            {project.lookingfor?.map((instrument) => (
-              <Chip
-                key={instrument}
-                color={
-                  highlightedInstruments.includes(instrument)
-                    ? "primary"
-                    : "default"
-                }
-                onClick={() => onInstrumentClick(instrument)}
-                label={instrument}
-              />
-            ))}
-          </Stack>
+                {project.lookingforstrict ? <LockIcon /> : <LockOpenIcon />}
+              </Tooltip>
+            </Stack>
+          )}
           <Typography variant="body2">
             {maxNCharacters(project.description, 300)}
           </Typography>
