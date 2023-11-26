1c11cd93a1025ba7f86731fef77e8190ff8fd763
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-11-25 20:10:13 -0800

    added commits to react activity calendar

--- client/pages/user/[username].tsx
+++ client/pages/user/[username].tsx
@@ -1,17 +1,25 @@
 import { useAuthContext } from "@/contexts/authContext";
 import { getUser } from "@/utils/apiUtils";
+import { commitHistoryToActivityCalendar } from "@/utils/calendarUtils";
+import { act } from "@testing-library/react";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ActivityCalendar, { Activity } from "react-activity-calendar";
 
 const UserProfile = () => {
   const router = useRouter();
   const { username } = router.query;
   const { isAuthenticated } = useAuthContext();
+  const [activityHistory, setActivityHistory] = useState<Activity[]>([]);
 
   useEffect(() => {
     const getUserContrubutions = async (username: string) => {
       const usercontributions = await getUser(username);
-      console.log(usercontributions);
+      const activityHistory: Activity[] =
+        commitHistoryToActivityCalendar(usercontributions);
+      console.log(activityHistory);
+
+      setActivityHistory(activityHistory);
     };
     if (username === undefined) return;
     if (typeof username === "string") {
@@ -20,7 +28,15 @@ const UserProfile = () => {
       console.log("Error: username is not a string");
     }
   }, [username]);
-  return <div>User Profile</div>;
+  return (
+    <div>
+      {activityHistory.length > 0 ? (
+        <ActivityCalendar data={activityHistory} weekStart={0} />
+      ) : (
+        <div>No activity</div>
+      )}
+    </div>
+  );
 };
 
 export default UserProfile;
---
+++ client/utils/calendarUtils.ts
@@ -0,0 +1,32 @@
+import {Activity} from "react-activity-calendar";
+
+const dateToString = (date: Date) => {
+  const formattedDate = date.toLocaleDateString('en-CA', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).replace(/\//g, '-');
+  return formattedDate;
+}
+
+export const commitHistoryToActivityCalendar = (contributionList: UserContribution[]) => {
+  console.log("commitHistoryToActivityCalendar", contributionList)
+  const today = new Date();
+  const lastYearToday = new Date();
+  lastYearToday.setFullYear(today.getFullYear() - 1);
+  const activityCalendar: Activity[] = [{date: dateToString(lastYearToday), count: 0, level: 0}, {date: dateToString(today), count: 0, level: 0}];
+  const datesAdded: {[idx:string]: boolean} = {};
+  contributionList.forEach((contribution) => {
+    const date = new Date(contribution.createdat);
+    const dateString = dateToString(date);
+    
+    // const date = new Date(dateString.replace(' ', 'T'));
+    if (datesAdded[dateString]) {
+      activityCalendar.find((activity) => activity.date === dateString)!.count++;
+    } else {
+      activityCalendar.push({date: dateString, count: 1, level: 1});
+      datesAdded[dateString] = true;
+    }
+  });
+  return activityCalendar;
+}
